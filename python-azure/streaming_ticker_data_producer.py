3f7948290216292cb7717d8a81af3943a16c5dd1
Sanjay Sengupta <sengupta.sanjay@gmail.com>
2022-05-30 17:06:28 -0400

    Changed an alias

--- python-azure/streaming_ticker_data_producer.py
+++ python-azure/streaming_ticker_data_producer.py
@@ -5,7 +5,7 @@ tl = ['aapl', 'amd', 'amzn', 'cmg', 'fb', 'googl', 'msft', 'nvda', 'shop', 'snap
 
 while True:
     for t in tl:
-        d = si.get_live_price(t)
-        tp = '{ ' + t + ' : ' + str(round(d, 2)) + ' }'
+        p = si.get_live_price(t)
+        tp = '{ ' + t + ' : ' + str(round(p, 2)) + ' }'
         print(tp.upper())
     time.sleep(5)
