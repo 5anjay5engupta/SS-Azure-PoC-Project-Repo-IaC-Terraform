411706a4ede727f15c5547dc8cd3c9a902b6a761
Sanjay Sengupta <sengupta.sanjay@gmail.com>
2022-05-30 04:32:11 -0400

    Added wait between feeds

--- python-azure/yahoo_ticker_data.py
+++ python-azure/yahoo_ticker_data.py
@@ -1,7 +1,11 @@
+import time
 from yahoo_fin import stock_info as si
 
 tl = ['aapl', 'amd', 'amzn', 'cmg', 'fb', 'googl', 'msft', 'nvda', 'shop', 'snap']
-for t in tl:
-    d = si.get_live_price(t)
-    tu = '{ ' + t + ' : ' + str(round(d, 2)) + ' }'
-    print(tu.upper())
+
+while True:
+    for t in tl:
+        d = si.get_live_price(t)
+        tp = '{ ' + t + ' : ' + str(round(d, 2)) + ' }'
+        print(tp.upper())
+    time.sleep(5)
