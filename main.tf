95b68dcda27e89e0b91b23a9c44983590f5a6878
Sanjay Sengupta <sengupta.sanjay@gmail.com>
2022-09-05 17:00:53 -0400

    trying something out

--- main.tf
+++ main.tf
@@ -132,10 +132,13 @@ resource "azurerm_user_assigned_identity" "idev_adf_v2_adls_gen2_uami" {
 }
 
 # create a data factory linked service to a adls gen2 store for the actual PoC stuff
+data "azurerm_client_config" "current" {
+}
+
 resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "idev_adf_v2_ls_adls_gen2" {
   name                  = "iDEV-ADF-V2-Linked-Service-ADLS_Gen2"
   data_factory_id       = azurerm_data_factory.idev_adf_v2_wksp.id
-  service_principal_id  = idev_adf_v2_adls_gen2_uami.id
+  service_principal_id  = data.azurerm_client_config.current.client_id
   # service_principal_key = "exampleKey"
   # tenant                = "11111111-1111-1111-1111-111111111111"
   url                   = "https://idevstorageaccount"
