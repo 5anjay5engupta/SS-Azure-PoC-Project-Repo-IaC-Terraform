29b343745cbf8bc6d6f141ea266e836b88a9fa07
5anjay5engupta <sengupta.sanjay@live.com>
2022-11-03 11:02:45 -0400

    Updated last modified date

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 /********************************************************************************/
 #       Author : Sanjay Sengupta - Enterprise Cloud & Big Data Analytics Architect
-# Last Updated : Oct 25, 2022
+# Last Updated : Nov 3, 2022
 /********************************************************************************/
 
 # configure the Azure provider
